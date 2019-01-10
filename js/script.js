607006b1f90dc67a36636f7cd8f4e81e5d52c85e
Achint Srivastava <achint.179301016@muj.manipal.edu>
2019-01-10 20:08:22 +0530

    snippets

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function () {
 
 	$("#navbarToggle").blur(function(event){
 		var screenWidth=window.innerWidth;
-		if(screenWidth<767) {
+		if(screenWidth<768) {
 			$("#collapsable-nav").collapse('hide');
 		}
 	});
@@ -10,3 +10,35 @@ $(function () {
 });
 // $ sign before a function does same as document.addEventListener("DOMContentLoaded"...
 // $ sign is also used as document.querySelector("#navbarToggle").addEventListener("blur,..
+
+(function (global){
+
+var dc={};
+
+var homeHtml= "Snippets/home-snippet.html";
+
+var insertHtml = function (selector, html) {
+	var targetElem = document.querySelector(selector);
+	targetElem.innerHTML = html;
+};
+
+var showLoading = function (selector) {
+	var html = "<div class='text-center'>";
+	html +="<img src='images/ajax-loader.gif'></div>";
+	insertHtml(selector, html);
+};
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+	homeHtml,
+	function(responseText) {
+		document.querySelector("#main-content")
+			.innerHTML = responseText;
+	},
+	false);
+});
+
+global.$dc=dc;
+})(window);
